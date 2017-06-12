cc31d8c70d7c896ecd8cc374d146b682880e5494
Pedro Martin <pataruco@gmail.com>
2017-06-12 21:05:48 +0100

    Create a controller to handle constituency post

--- controllers/member-controller.js
+++ controllers/member-controller.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-// const LetterList = require('../models/letter-list.js');
 
 
 router.get('/', (req, res) => {
-    console.log('Redirect to /a');
     res.render('index');
+});
+
+router.post('/getMPby=:constituency', ( req, res ) => {
+    let constituency = req.params.constituency;
+    console.log(constituency);
 });
 
 
