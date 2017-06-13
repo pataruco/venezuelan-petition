dbec6f671d76984a83f05eb3f5754e5be3722abf
Pedro Martin <pataruco@gmail.com>
2017-06-13 13:30:15 +0100

    change jquery plugin

---
+++ assets/js/vendor/is-on-screen.js
@@ -0,0 +1,52 @@
+;(function ($) {
+
+    /**
+    * Tests if a node is positioned within the current viewport.
+    * It does not test any other type of "visibility", like css display,
+    * opacity, presence in the dom, etc - it only considers position.
+    *
+    * By default, it tests if at least 1 pixel is showing, regardless of
+    * orientation - however an optional argument is accepted, a callback
+    * that is passed the number of pixels visible on each edge - the return
+    * (true of false) of that callback is used instead.
+    */
+    $.fn.isOnScreen = function(test){
+
+        var height = this.outerHeight();
+        var width = this.outerWidth();
+
+        if(!width || !height){
+            return false;
+        }
+
+        var win = $(window);
+
+        var viewport = {
+            top : win.scrollTop(),
+            left : win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+
+        var bounds = this.offset();
+        bounds.right = bounds.left + width;
+        bounds.bottom = bounds.top + height;
+
+        var showing = {
+          top : viewport.bottom - bounds.top,
+          left: viewport.right - bounds.left,
+          bottom: bounds.bottom - viewport.top,
+          right: bounds.right - viewport.left
+        };
+
+        if(typeof test == 'function') {
+          return test(showing);
+        }
+
+        return showing.top > 0
+          && showing.left > 0
+          && showing.right > 0
+          && showing.bottom > 0;
+    };
+
+})(jQuery);
