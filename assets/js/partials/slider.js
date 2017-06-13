73fe6e7d69f70c9fa67d1705b701650077e3698c
Pedro Martin <pataruco@gmail.com>
2017-06-13 17:42:54 +0100

    Instantiate slick slider

--- assets/js/partials/slider.js
+++ assets/js/partials/slider.js
@@ -1,6 +1,8 @@
 $( document ).ready( ( ) => {
     const $slider = $('#js-slider');
     $slider.slick({
-        // adaptiveHeight: true
+        adaptiveHeight: true,
+        dots: true,
+        arrows: true
     });
 });
