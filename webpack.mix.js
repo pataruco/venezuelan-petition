78b2840fc2a4aa8aa20d5a3231683775196a3436
Pedro Martin <pataruco@gmail.com>
2017-06-14 20:49:35 +0100

    Fix Compile bug

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,9 @@
 const mix  = require('laravel-mix');
 
 mix.js('assets/js/app.js', 'public/js')
-   .sass('assets/sass/app.scss', 'public/css');
+    .sass('assets/sass/app.scss', 'public/css')
+    .copyDirectory('node_modules/slick-carousel/slick/fonts', 'public/css/fonts')
+    .copy('node_modules/slick-carousel/slick/ajax-loader.gif', 'public/css/ajax-loader.gif')
+    .options({
+        processCssUrls: false
+    });
