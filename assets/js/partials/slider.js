c356b8ea8da3c8714e60666684c272e7fd0f2b19
Pedro Martin <pataruco@gmail.com>
2017-06-29 21:42:32 +0100

    Append custom arrows to slick slider

--- assets/js/partials/slider.js
+++ assets/js/partials/slider.js
@@ -4,5 +4,7 @@ $( document ).ready( ( ) => {
         adaptiveHeight: true,
         dots: true,
         arrows: true,
+        prevArrow: '<button class="slider__arrow slider__arrow--left"></button>',
+        nextArrow: '<button class="slider__arrow slider__arrow--right"></button>'
     });
 });
