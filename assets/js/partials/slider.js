49a2c7efdced53ad4e4bc4b5dae04bb094b6d1e4
Pedro Martin <pataruco@gmail.com>
2017-07-02 20:48:09 +0100

    Add breakpoints to slider

--- assets/js/partials/slider.js
+++ assets/js/partials/slider.js
@@ -5,6 +5,22 @@ $( document ).ready( ( ) => {
         dots: true,
         arrows: true,
         prevArrow: '<button class="slider__arrow slider__arrow--left"></button>',
-        nextArrow: '<button class="slider__arrow slider__arrow--right"></button>'
+        nextArrow: '<button class="slider__arrow slider__arrow--right"></button>',
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    adaptiveHeight: false
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    adaptiveHeight: false,
+                    dots: false
+                }
+            }
+        ]
     });
 });
